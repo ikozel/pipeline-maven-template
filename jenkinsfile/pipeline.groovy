e3d15a2f0195eba9ac7e2180a38398167722eac6
Igor Kozel <igorkozel@Igors-MacBook-Pro.local>
2017-07-02 12:08:57 -0700

    goals

--- jenkinsfile/pipeline.groovy
+++ jenkinsfile/pipeline.groovy
@@ -3,6 +3,12 @@ node {
                 checkout scm
         }
         stage('Build') {
-                sh 'pwd'
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn clean compile'
+        }
+        stage('Test') {
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn test'
+        }
+        stage('Package') {
+                sh '~/workspace/apache-maven-3.5.0/bin/mvn package'
         }
 }
